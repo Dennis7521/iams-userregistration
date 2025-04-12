683e52eb09fbee46289df6b55a9d9a68773f4ae5
Dennis7521 <mothusigaamangwe64@gmail.com>
2025-04-12 17:22:37 +0200

    Update HomePage.jsx

--- src/component/pages/HomePage.jsx
+++ src/component/pages/HomePage.jsx
@@ -3,7 +3,6 @@ const HomePage = () => {
     return (
       <div style={{ padding: '2rem' }}>
         <h1>Welcome to IAMS</h1>
-        <p>Select an option from the menu</p>
       </div>
     );
   };
