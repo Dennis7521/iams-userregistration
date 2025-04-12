2cc0d143e8fb66415827d4afe8b8d1624015e039
Dennis7521 <mothusigaamangwe64@gmail.com>
2025-04-12 17:50:59 +0200

    Update MainLayout.jsx

--- src/component/layout/MainLayout.jsx
+++ src/component/layout/MainLayout.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Outlet, useNavigate } from 'react-router-dom';
+import {Link, Outlet, useNavigate } from 'react-router-dom';
 
 const MainLayout = () => {
   const [hoveredItem, setHoveredItem] = useState(null);
